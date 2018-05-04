e3c941a621f45437a7aefc18da610374d8dcdee3
lifan <fan.li@1hash.com>
2018-05-04 15:16:48 +0800

    单个卡片数据展示

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import { Row, Col } from 'antd';
+import { Row, Col, Modal } from 'antd';
 
 class HomePage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      visible: false,
+      modalData: {}
+    }
+  }
+
+  orderList(item){
+    this.setState({
+      visible: true,
+      modalData: item
+    });
+  }
+
+  handleOk(){
+    this.setState({
+      visible: false
+    });
+  }
+
+  handleCancel(){
+    this.setState({
+      visible: false
+    });
+  }
+
   render() {
     const {allNews} = this.props.homeReducers;
+    const {modalData} = this.state;
     let result = (allNews.data && allNews.data.results) && allNews.data.results;
     console.log(result,'homeR-222>>>');
     let colElement = result && result.length > 0 && result.map((item, index) => {
       return (
-        <Col key={index} className="news-top-col" span={8}>
+        <Col key={index} className="news-top-col" span={8} onClick={() => this.orderList(item)}>
           <div className="col-list">
             <div className="col-list-container">
               <Row className="row-header">
@@ -39,6 +67,43 @@ class HomePage extends Component {
         <Row className="news-top-row">
           {colElement}
         </Row>
+        <Modal
+          title={modalData.name}
+          visible={this.state.visible}
+          onOk={() => this.handleOk()}
+          onCancel={() => this.handleCancel()}
+        >
+          <Row>
+            <Col span={12}>
+              <span className='order-left-label'>订单ID: </span>
+              <span className="order-right-label">{modalData.id}</span>
+            </Col>
+            <Col span={12}>
+              <span className='order-left-label'>费率: </span>
+              <span className="order-right-label">{modalData.manage_fee}</span>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <span className='order-left-label'>价格: </span>
+              <span className="order-right-label">{modalData.sell_price}</span>
+            </Col>
+            <Col span={12}>
+              <span className='order-left-label'>功耗: </span>
+              <span className="order-right-label">{modalData.power_fee}</span>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <span className='order-left-label'>汇率: </span>
+              <span className="order-right-label">{modalData.power_rate}</span>
+            </Col>
+            <Col span={12}>
+              <span className='order-left-label'>库存: </span>
+              <span className="order-right-label">{modalData.store}</span>
+            </Col>
+          </Row>
+        </Modal>
       </div>
     )
   }
--- src/redux/reducers/homeReducers.js
+++ src/redux/reducers/homeReducers.js
@@ -9,7 +9,6 @@ let homeInit = {
 export default function HomeReducers(state = homeInit, action) {
   switch (action.type) {
     case types.GET_ALL_NEWS:
-      console.log(action.data,'action.data')
       return Object.assign({}, state, {allNews: action.data});
     default:
       return state;
